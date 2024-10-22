57cc2d879816e9efbd46d1627f1d05518701abfc
sunwoo020 <sunwoo020@naver.com>
2024-10-22 22:52:24 +0900

    Refactor:NextAuth 리팩토링

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,41 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import KakaoProvider from "next-auth/providers/kakao";
 import NaverProvider from "next-auth/providers/naver";
 
+const ERROR_MESSAGES = {
+  INVALID_CREDENTIALS: "Invalid credentials",
+  AUTH_FAILED: "로그인에 실패하였습니다. 사용자 자격증명을 확인해주세요.",
+};
+
+async function authenticateUser(username: string, password: string) {
+  try {
+    const { data } = await axiosInstance.post(
+      "/auth/login",
+      {},
+      {
+        auth: {
+          username,
+          password,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    // console.error("Failed to authenticate user:", error);
+    throw new Error(ERROR_MESSAGES.AUTH_FAILED);
+  }
+}
+
+const PROVIDER_CONFIGS = {
+  NAVER: {
+    clientId: process.env.NAVER_CLIENT_ID!,
+    clientSecret: process.env.NAVER_CLIENT_SECRET!,
+  },
+  KAKAO: {
+    clientId: process.env.KAKAO_CLIENT_ID!,
+    clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+  },
+};
+
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
@@ -16,50 +51,37 @@ const handler = NextAuth({
         },
         password: { label: "비밀번호", type: "password" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         if (!credentials) {
-          throw new Error("Invalid credentials");
+          throw new Error(ERROR_MESSAGES.INVALID_CREDENTIALS);
         }
 
-        const { data } = await axiosInstance.post(
-          "/auth/login",
-          {},
-          {
-            auth: {
-              username: credentials?.username,
-              password: credentials?.password,
-            },
-          }
+        const data = await authenticateUser(
+          credentials.username,
+          credentials.password
         );
         if (data) {
           return { ...data, accessToken: data.accessToken };
-        } else {
-          return null;
         }
+        return null;
       },
     }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID!,
-      clientSecret: process.env.NAVER_CLIENT_SECRET!,
-    }),
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID!,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
-    }),
+    NaverProvider(PROVIDER_CONFIGS.NAVER),
+    KakaoProvider(PROVIDER_CONFIGS.KAKAO),
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }): Promise<any> {
       if (user) {
         token.accessToken = user.accessToken;
       }
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }): Promise<any> {
       session.user = token;
       session.accessToken = token.accessToken;
       return session;
     },
-    async redirect({ url, baseUrl }) {
+    async redirect({ baseUrl }): Promise<string> {
       return baseUrl;
     },
   },
